21bcd78f51923a311a6e83e824a50eb07bf55857
Miguel Palhas <mpalhas@gmail.com>
2023-12-15 09:17:40 +0000

    code review

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -7,25 +7,17 @@ use tower_http::cors::CorsLayer;
 use tracing::instrument;
 
 use crate::{config::HttpConfig, db::Db};
-use axum::{
-    routing::{get, post},
-    Router,
-};
 
-use self::routes::{health, register};
+use self::routes::router;
 
 #[allow(clippy::async_yields_async)]
 #[instrument(name = "api", skip(db, config), fields(port = config.port))]
 pub async fn start(db: Db, config: HttpConfig) -> JoinHandle<Result<(), std::io::Error>> {
-    let app: Router = Router::new()
-        .route("/health", get(health))
-        .route("/register", post(register))
-        .layer(CorsLayer::permissive())
-        .with_state(db);
+    tokio::spawn(async move {
+        let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
+        let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
-
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-
-    tokio::spawn(async move { axum::serve(listener, app).await })
+        let app = router().layer(CorsLayer::permissive()).with_state(db);
+        axum::serve(listener, app).await
+    })
 }
--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,17 +1,25 @@
 use super::error::Result;
 use crate::db::Db;
 use axum::extract::State;
+use axum::routing::{get, post};
+use axum::Router;
 use axum::{response::IntoResponse, Json};
 use serde::Deserialize;
 
-pub async fn health() -> impl IntoResponse {}
+pub fn router() -> Router<Db> {
+    Router::new()
+        .route("/health", get(health))
+        .route("/register", post(register))
+}
+
+async fn health() -> impl IntoResponse {}
 
 #[derive(Debug, Deserialize)]
-pub struct Register {
+struct Register {
     pub address: alloy_primitives::Address,
 }
 
-pub async fn register(
+async fn register(
     State(db): State<Db>,
     Json(register): Json<Register>,
 ) -> Result<impl IntoResponse> {
