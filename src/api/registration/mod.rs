82df0d354cbc929450a723d9463047daec9a04d6
Miguel Palhas <mpalhas@gmail.com>
2024-08-06 11:53:00 +0100

    code review

--- src/api/registration/mod.rs
+++ src/api/registration/mod.rs
@@ -1,6 +1,6 @@
 use color_eyre::{eyre::eyre, Result};
-use reth_primitives::{Address, TxHash};
-use reth_provider::ReceiptProvider as _;
+use reth_primitives::{Address, TransactionSigned, TxHash};
+use reth_provider::TransactionsProvider as _;
 use serde::{Deserialize, Serialize};
 
 use super::app_state::AppState;
@@ -27,14 +27,43 @@ impl RegistrationProof {
 
             Self::TxHash(hash) => {
                 let provider = state.provider_factory.get()?;
-                let receipt = provider.receipt_by_hash(*hash)?;
-                dbg!(receipt);
-                todo!()
+                match provider.transaction_by_hash(*hash)? {
+                    Some(tx) => self.validate_tx(address, state, &tx)?,
+                    None => return Err(eyre!("Transaction not found")),
+                }
             }
 
             #[cfg(test)]
             Self::Test => return Ok(()),
         };
+
+        Ok(())
+    }
+
+    fn validate_tx(
+        &self,
+        address: Address,
+        state: &AppState,
+        tx: &TransactionSigned,
+    ) -> Result<()> {
+        if tx.recover_signer() != Some(address) {
+            return Err(eyre!("Transaction origin does not match given address"));
+        }
+
+        let Some(payment_config) = state.config.payment else {
+            return Ok(());
+        };
+
+        if tx.to() != Some(payment_config.address) {
+            return Err(eyre!("Transaction must be sent to the payment address"));
+        }
+
+        if tx.value() < payment_config.min_amount {
+            return Err(eyre!(
+                "Transaction value must be at least {}",
+                payment_config.min_amount
+            ));
+        }
 
         Ok(())
     }
